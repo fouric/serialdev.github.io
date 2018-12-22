b6bdbc7e5acc51437776ac3fc725fdbf0fb34f30
Andres Mariscal <carlos.mariscal.melgar@gmail.com>
2018-12-22 01:35:08 +0000

    [+] Lisp markdown table generator

---
+++ main.lisp
@@ -0,0 +1,64 @@
+
+(defun concatString (list)
+  "A non-recursive function that concatenates a list of strings."
+  (if (listp list)
+      (with-output-to-string (s)
+         (dolist (item list)
+           (if (stringp item)
+             (format s "~a" item))))))
+
+(setq cols '("Title" "Author" "URL" "Topic" "Book" "Blog" "Theory"
+ "Implementation" "pseudocode" "List" "Free" "End-To-End"))
+
+
+(setq entry-list '(
+("Stackoverflow compilers" "miscellaneous" "www.hhh.com" "compilers" "t" "t" "t" "t" "t" "t" "t" "t")
+("ssssssssssssssscompilerssss" "miscellaneous" "www.hhh.com" "compilers" "t" "t" "t" "t" "t" "t" "t" "t")
+))
+
+(setq cwd (sb-posix:getcwd))
+
+(with-open-file (md  (concatenate 'string cwd "/README.md")
+                     :direction :output
+                     :if-exists :supersede
+                     :if-does-not-exist :create)
+
+
+(let ((max-length 0))
+  (loop for entry in entry-list
+     do (loop for i in entry do(setq max-length (if (< max-length  (length  i))
+			     (length i )(eval max-length)))
+))
+(setq max-length (+ max-length 2))
+
+  (format md (format nil "|~~~:@<~A~>:@<~~A~~>|" (- max-length 2)) (car cols))
+
+  (loop for current in (cdr cols)
+  do (progn
+
+  (format md (format nil "~~~:@<~A~>:@<~~A~~>|" (- max-length 1)) current)))
+
+  (format md "~%")
+  (format md "|~v{~A~:*~}:|" (- max-length 3) '("-"))
+    (loop for current in (cdr cols)
+    do (progn
+    (format md ":~v{~A~:*~}:|" (- max-length 3) '("-"))))
+
+
+
+(loop for entry in entry-list do 
+  (progn(
+  format md "~%")
+  (format md (format nil "|~~~:@<~A~>:@<~~A~~>|" (- max-length 2)) (car entry))
+
+    (loop for i in (cdr entry) do
+  (format md (format nil "~~~:@<~A~>:@<~~A~~>|" (- max-length 1)) i)))
+)
+
+))
+
+
+
+
+
+
